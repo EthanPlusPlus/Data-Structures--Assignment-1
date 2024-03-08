cefa173ed7bdd4f4826f43eb28d84edc0f5c6e78
EthanPlusPlus <ethann.pillayy@gmail.com>
2024-03-08 23:07:36 +0200

    Preparing for Makefile; Deleted testing stuff

---
+++ GenericsKbApp/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Display"},{"p":"<Unnamed>","l":"KnowledgeBase"},{"p":"<Unnamed>","l":"Record"}];updateSearchResults();
